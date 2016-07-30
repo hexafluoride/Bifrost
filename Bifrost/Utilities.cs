e11c060adf1c4baa87021d0ff93cde7b3037a7e0
hexafluoride <xafluoride@gmail.com>
2016-07-30 13:10:02 +0300

    Tweak version behavior a bit

--- Bifrost/Utilities.cs
+++ Bifrost/Utilities.cs
@@ -29,6 +29,21 @@ namespace Bifrost
         public static void LogVersion()
         {
             string version = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+
+            // truncate git hash to 8 chars for readability
+            var parts = version.Split('-');
+
+            if (parts.Length > 1)
+            {
+                parts[parts.Length - 1] = parts[parts.Length - 1].Substring(0, 8);
+                version = string.Join("-", parts);
+            }
+
+            version = "\"" + version + "\"";
+
+            if (parts.Length <= 1)
+                version += " (unrecognized version, not an AppVeyor build)";
+
             Log.Info("Bifrost version {0}", version);
         }
     }
